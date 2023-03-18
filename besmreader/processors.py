b354821ff5ee567382d6b405456fc6dd255c171c
Vivien Boistuaud <vivien.b@outlook.be>
2023-03-18 19:36:51 +0100

    Added P1Processor using Python logging

--- besmreader/processors.py
+++ besmreader/processors.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 from paho.mqtt import client as paho
 
 from .helper import LoggedClass
+import logging
 import ssl
 import os
 
@@ -13,13 +14,13 @@ class P1Processor:
         Interface for all P1 Port information processors
     """
 
-    def __init__(self, processorConfig: str):
-        self.processorConfig = processorConfig
+    def __init__(self, processorConfig: dict):
+        self._processorConfig = processorConfig
 
     def processSequence(self, p1Sequence: P1Sequence, applyTo: dict):
         for label in applyTo:
             if (p1Sequence.hasInformation(label)):
-                self.processInformation(self.processorConfig["topics"][label], p1Sequence.getInformationValue(label))
+                self.processInformation(self._processorConfig["topics"][label], p1Sequence.getInformationValue(label))
     
     @abstractmethod
     def processInformation(self, processLabel: str, processValue: str):
@@ -34,26 +35,46 @@ class P1Processor:
     def getConfigurationName() -> str:
         pass
 
-class PrintP1Processor(P1Processor):
+class PrintP1Processor (P1Processor):
 
     """
         A P1 Port Information processor that prints data to stdout
     """
 
-    def __init__(self, processorConfig: str):
+    def __init__(self, processorConfig: dict):
         super().__init__(processorConfig)
 
     def processInformation(self, processLabel: str, processValue: str):
         print(processLabel + " = " + str(processValue))
 
     def closeProcessor(self):
-        print("--- end processor ---")
+        pass
     
     @staticmethod
     def getConfigurationName() -> str:
         return "print"
 
-class MQTTP1Processor(P1Processor, LoggedClass):
+class LoggerP1Processor (P1Processor, LoggedClass):
+
+    def __init__(self, processorConfig: dict):
+        if (not "logLevel" in processorConfig):
+            processorConfig["logLevel"] = "INFO"
+        self._loggerLevel = logging.__dict__[processorConfig["logLevel"]]
+        P1Processor.__init__(self, processorConfig)
+        LoggedClass.__init__(self)
+
+    def processInformation(self, processLabel: str, processValue: str):
+        super().logger.log(self._loggerLevel, '%s: %s', processLabel, str(processValue))
+
+    def closeProcessor(self):
+        pass
+
+    @staticmethod
+    def getConfigurationName() -> str:
+        return "logger"
+
+
+class MQTTP1Processor (P1Processor, LoggedClass):
 
     """
         A P1 Port Information processor that sends data to MQTT
@@ -66,8 +87,10 @@ class MQTTP1Processor(P1Processor, LoggedClass):
         else:
             super().logger.error("MQTT: Bad connection Returned code rc=", rc)
 
-    def __init__(self, processorConfig: str):
-        super().__init__(processorConfig)
+    def __init__(self, processorConfig: dict):
+        P1Processor.__init__(self, processorConfig)
+        LoggedClass.__init__(self)
+
         self.mqttClient = paho.Client(client_id="belgian-smartmeter-p1-to-mqtt")
         self.mqttClient.on_connect = MQTTP1Processor.on_connect
         
@@ -75,11 +98,11 @@ class MQTTP1Processor(P1Processor, LoggedClass):
         self.mqttClient.tls_set(ca_certs=crtFileName, tls_version=ssl.PROTOCOL_TLSv1_2, cert_reqs=ssl.CERT_NONE)
         self.mqttClient.tls_insecure_set(True)
         
-        self.mqttClient.username_pw_set(self.processorConfig['username'], self.processorConfig['password'])
+        self.mqttClient.username_pw_set(self._processorConfig['username'], self._processorConfig['password'])
         self.connectMQTT()
 
     def connectMQTT(self):
-        self.mqttClient.connect(self.processorConfig['broker'], self.processorConfig['tcpPort'], 60)    
+        self.mqttClient.connect(self._processorConfig['broker'], self._processorConfig['tcpPort'], 60)    
 
     def processInformation(self, processLabel: str, processValue: str):
         if (not self.mqttClient.is_connected):
@@ -98,7 +121,7 @@ class P1ProcessorFactory:
     """
         A factory for creating P1 processors from a configuration.
     """
-    _processorClassList = [MQTTP1Processor, PrintP1Processor]
+    _processorClassList = [MQTTP1Processor, PrintP1Processor, LoggerP1Processor]
     _procesorDictionary = None
 
     @classmethod
