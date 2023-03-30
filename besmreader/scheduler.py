bc5e86374eade4451ec047edc4fff8b5dc72a8dc
Vivien Boistuaud <vivien.b@outlook.be>
2023-03-30 22:04:49 +0200

    Added "changed" potential type of scheduler contributes to #7 task 1

--- besmreader/scheduler.py
+++ besmreader/scheduler.py
@@ -9,6 +9,7 @@ class P1Scheduler:
     def __init__(self, config):
         self.__schedules = config.scheduling
         self.__config = config
+        self.__cachedSequences = dict()
 
     def processP1(self, p1Sequence: P1Sequence) -> None:
         if (not p1Sequence.hasTimeinSystemTimezone):
@@ -16,20 +17,34 @@ class P1Scheduler:
         
         p1Sequence.applyTransformations()
 
-        for schedule in self.__schedules:
+        scheduleIndex = 0
+        while (scheduleIndex < len(self.__schedules)):
+            schedule = self.__schedules[scheduleIndex]
+            applyToSchedule = schedule["applyTo"]
+
             if (p1Sequence.messageTimeinSystemTimezone >= schedule["cron_next_trigger"]):
                 self.processor = self.__config.getProcessor(schedule["processor"])
 
                 if (schedule["mode"] == "average"):
-                    for obisId in schedule["applyTo"]:
+                    for obisId in applyToSchedule:
                         if (len(schedule["history"][obisId])>0):
                             p1Sequence.addInformation(obisId, round(mean(schedule["history"][obisId]),3), p1Sequence.getInformationUnit(obisId))
                         schedule["history"][obisId].clear()
 
-                self.processor.processSequence(p1Sequence, schedule["applyTo"])
+                if (schedule["mode"] == "changed"):
+                    formerSequence = self.__cachedSequences.get(scheduleIndex)
+                    if (not formerSequence is None):
+                        # TODO
+                        pass
+                    self.__cachedSequences[scheduleIndex] = p1Sequence
+                    pass
+
+                self.processor.processSequence(p1Sequence, applyToSchedule)
+                #self.__cachedSequences[schedule]
                 schedule["cron_next_trigger"] = schedule["cron"].get_next(datetime)
             else:
                 if (schedule["mode"] == "average"):
-                    for obisId in schedule["applyTo"]:
+                    for obisId in applyToSchedule:
                         if (p1Sequence.hasInformation(obisId)):
                             schedule["history"][obisId].append(p1Sequence.getInformationValue(obisId))
+            scheduleIndex += 1
